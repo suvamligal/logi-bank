614df19a7b5a60560ae4901ed4fca5c4704b8b30
suvamligal <suvamligal@gmail.com>
2022-11-01 16:46:43 -0500

    initial commit

---
+++ src/main/java/com/lashoo/logibank/LogiBankApplication.java
@@ -0,0 +1,13 @@
+package com.lashoo.logibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogiBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lashoo/logibank/LogiBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lashoo.logibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
